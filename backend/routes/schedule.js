7460b334c421510eee13e73de480e9020c741d8f
jdtheefirst <jngatia045@gmail.com>
2024-12-26 18:07:41 +0300

    returning false/true

--- backend/routes/schedule.js
+++ backend/routes/schedule.js
@@ -117,11 +117,17 @@ router.get("/room", limiter, async (req, res) => {
     const endTime = new Date(roomDetails.endTime);
     const now = new Date();
 
+    console.log("Current Time:", now);
+    console.log("Start Time:", startTime);
+    console.log("End Time:", endTime);
+
     // Check if the current time is within the event's duration
     if (now >= startTime && now <= endTime) {
+      console.log("Returning true...");
       roomDetails.isLive = true;
     } else {
       roomDetails.isLive = false;
+      console.log("Returning false...");
     }
 
     res.status(200).json(roomDetails);
